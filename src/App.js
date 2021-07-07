b742108cdae33bd4e1629b315b2819aaa96253a9
khovricheva <kseniyap1102@gmail.com>
2021-07-07 16:33:25 +0300

    feat: add sort a table by clicking its headers

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import Loader from './Loader/Loader';
 import Table from './Table/Table';
 import { useState, useEffect } from 'react';
+import _ from 'lodash';
 
 function App() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortDir, setSortDir] = useState('asc');
+  const [sortField, setSortField] = useState('');
   // const [state, setstate] = useState(initialState)
-  // const [state, setstate] = useState(initialState)
-  // const [state, setstate] = useState(initialState)
+  const onSort = (sortField) => {
+    const copiedData = data.concat();
+    const sortType = sortDir === 'asc' ? 'desc' : 'asc';
+
+    const orderedData = _.orderBy(copiedData, sortField, sortType);
+    setData(orderedData);
+    setSortDir(sortType);
+    setSortField(sortField);
+  };
 
   const fetchData = async () => {
     const response = await fetch(
@@ -27,7 +37,16 @@ function App() {
 
   return (
     <div className='container'>
-      {isLoading ? <Loader /> : <Table data={data} />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Table
+          data={data}
+          onSort={onSort}
+          sortDir={sortDir}
+          sortField={sortField}
+        />
+      )}
     </div>
   );
 }
--- src/Table/Table.js
+++ src/Table/Table.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Table.css';
 
-function Table({ data }) {
+function Table({ data, onSort, sortDir, sortField }) {
   return (
     <div>
       <table className='table'>
         <thead>
           <tr>
-            <th>ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Phone</th>
+            <th onClick={onSort.bind(null, 'id')}>
+              ID {sortField === 'id' ? <small>{sortDir}</small> : null}
+            </th>
+            <th onClick={onSort.bind(null, 'firstName')}>
+              First Name{' '}
+              {sortField === 'firstName' ? <small>{sortDir}</small> : null}
+            </th>
+            <th onClick={onSort.bind(null, 'lastName')}>
+              Last Name{' '}
+              {sortField === 'lastName' ? <small>{sortDir}</small> : null}
+            </th>
+            <th onClick={onSort.bind(null, 'email')}>
+              Email {sortField === 'email' ? <small>{sortDir}</small> : null}
+            </th>
+            <th onClick={onSort.bind(null, 'phone')}>
+              Phone {sortField === 'phone' ? <small>{sortDir}</small> : null}
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -19,6 +32,7 @@ function Table({ data }) {
               <td>{item.id}</td>
               <td>{item.firstName}</td>
               <td>{item.lastName}</td>
+              <td>{item.email}</td>
               <td>{item.phone}</td>
             </tr>
           ))}
